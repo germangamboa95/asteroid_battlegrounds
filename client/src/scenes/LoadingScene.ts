11c264650bb89694141a5665e2f33d40c34dd32e
annaelde <elde.av@gmail.com>
2019-09-28 16:51:53 -0400

    Move assets into dist.

--- client/src/scenes/LoadingScene.ts
+++ client/src/scenes/LoadingScene.ts
@@ -1,19 +1,55 @@
 import { Scene } from "phaser";
+import {CLIENT_HEIGHT, CLIENT_WIDTH} from "../index";
 
 export class LoadingScene extends Scene {
-  public constructor() {
-    super({ key: "LoadingScene" });
+  private movieFrame: Phaser.GameObjects.Image
+  private movieTexture: Phaser.Textures.CanvasTexture
+  private video: HTMLVideoElement
+
+  constructor() {
+    super({ key: 'LoadingScene' })
   }
 
-  public preload() {
-    // Load assets here
+  preload() {
+    this.load.image('background', '/assets/star_gold.png');
   }
 
-  public create() {
-    // Construct world
+  create() {
+    this.movieTexture = this.textures.createCanvas('movie', CLIENT_WIDTH, CLIENT_HEIGHT)
+    this.add.image(400, 300, 'background');
+    // this.movieFrame = this.add.image(CLIENT_WIDTH / 2, CLIENT_HEIGHT / 2, 'movie').setInteractive()
+    // this.video = document.createElement('video')
+    // this.video.src = 'assets/videos/intro.mp4'
+    // const game = this
+    // this.video.addEventListener('loadeddata', function() {
+    //   this.play()
+    //   const fps = 30
+    //   const loop = () => {
+    //     if (!this.paused && !this.ended) {
+    //       game.movieTexture.context.drawImage(this, 0, 0, CLIENT_WIDTH, CLIENT_HEIGHT)
+    //       game.movieTexture.refresh()
+    //       setTimeout(loop, 1000/fps)
+    //     }
+    //   }
+    //   loop();
+    // })
+    // this.video.addEventListener('ended', function() {
+    //   game.goNextScene()
+    // })
+    // this.video.addEventListener('pause', function() {
+    //   game.goNextScene()
+    // })
+    // this.movieFrame.on('pointerdown', () => {
+    //   this.video.pause()
+    // })
   }
 
-  public update() {
-    // update stuff
+  goNextScene() {
+    // this.video.remove()
+    // this.movieTexture.destroy()
+    this.scene.switch('RegisterScene')
+  }
+
+  update() {
   }
 }
