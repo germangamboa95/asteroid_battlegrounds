a1579baad719919643e8d97a920e5aa57c86f73b
German Gamboa <thegerman@MacBook-Pro-3.local>
2019-09-28 19:15:58 -0400

    it kinda works now

--- client/src/scenes/MainGame.ts
+++ client/src/scenes/MainGame.ts
@@ -2,6 +2,9 @@ import { Phaser, Scene } from "phaser";
 
 export class MainGame extends Scene {
   protected room: any;
+  protected sprite: Phaser.GameObjects.Sprite;
+  protected player: any;
+  protected cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
   public constructor(room: any) {
     super({ key: "MainGame" });
@@ -14,22 +17,48 @@ export class MainGame extends Scene {
   }
 
   public create() {
-    // Construct world
-    this.input.keyboard.on("keyup_W", event => {
-      this.room.send({ event: "W" });
-    });
-    this.input.keyboard.on("keyup_D", event => {
-      this.room.send({ event: "D" });
-    });
-    this.input.keyboard.on("keyup_A", event => {
-      this.room.send({ event: "A" });
-    });
-    this.input.keyboard.on("keyup_S", event => {
-      this.room.send({ event: "S" });
-    });
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.player = this.physics.add
+      .image(0, 0, "ship")
+      .setOrigin(0.5, 0.5)
+      .setDisplaySize(53, 40);
+    this.player.setDrag(100);
+    this.player.setAngularDrag(100);
+    this.player.setMaxVelocity(200);
+
+    this.room.state.players.onAdd = (player, key) => {
+      console.log(player, "has been added at", key);
+
+      // add your player entity to the game world!
+
+      // If you want to track changes on a child object inside a map, this is a common pattern:
+      player.onChange = function(changes) {
+        changes.forEach(change => {
+          console.log(change.field);
+          console.log(change.value);
+          console.log(change.previousValue);
+        });
+      };
+
+      // force "onChange" to be called immediatelly
+      player.triggerAll();
+    };
   }
 
   public update() {
-    // update stuff
+    if (this.cursors.left.isDown) {
+      this.room.send({ x: -1 });
+    }
+    if (this.cursors.right.isDown) {
+      this.room.send({ x: 1 });
+    }
+    if (this.cursors.up.isDown) {
+      this.room.send({ y: -1 });
+    }
+    if (this.cursors.down.isDown) {
+      this.room.send({ y: 1 });
+    }
+    this.player.x = this.room.state.players[this.room.sessionId].x;
+    this.player.y = this.room.state.players[this.room.sessionId].y;
   }
 }
