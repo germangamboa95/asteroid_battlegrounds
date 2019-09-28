91bf247fa43b2a4943701f51e618326b56144603
Nils Elde <nils.elde@gmail.com>
2019-09-28 16:01:47 -0400

    add audio to preload

--- client/src/scenes/MainGame.ts
+++ client/src/scenes/MainGame.ts
@@ -7,6 +7,17 @@ export class MainGame extends Scene {
 
   public preload() {
     // Load assets here
+    this.load.audio('bass', 'assets/music/stage_01.mp3');
+    this.load.audio('bass', 'assets/sfx/asteroid_explode.mp3');
+    this.load.audio('bass', 'assets/sfx/defeat.mp3');
+    this.load.audio('bass', 'assets/sfx/laser.mp3');
+    this.load.audio('bass', 'assets/sfx/missile_launch.mp3');
+    this.load.audio('bass', 'assets/sfx/players_get_ready.mp3');
+    this.load.audio('bass', 'assets/sfx/powerup.mp3');
+    this.load.audio('bass', 'assets/sfx/ship_explode.mp3');
+    this.load.audio('bass', 'assets/sfx/ship_hit.mp3');
+    this.load.audio('bass', 'assets/sfx/victory.mp3');
+
   }
 
   public create() {
