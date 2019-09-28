919035a138785a03d72bbd04780c3c322ae4a805
German Gamboa <thegerman@MacBook-Pro-3.local>
2019-09-28 15:58:51 -0400

    an update

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,4 @@
+import "babel-polyfill";
 import * as Colyseus from "colyseus.js";
 import Phaser from "phaser";
 import { EndGameScene } from "./scenes/EndGameScene";
@@ -6,37 +7,31 @@ import { LobbyScene } from "./scenes/LobbyScene";
 import { MainGame } from "./scenes/MainGame";
 import { RegisterScene } from "./scenes/RegisterScene";
 
-let client: Colyseus.Client = new Colyseus.Client("ws://localhost:2567");
+// let client: Colyseus.Client = new Colyseus.Client("ws://localhost:2567");
 
-client
-  .joinOrCreate("chat")
-  .then((room: Colyseus.Room) => {
-    console.log(room.state);
-    room.state.onChange = changes => {
-      changes.forEach(change => {
-        console.log(change.field);
-        console.log(change.value);
-        console.log(change.previousValue);
-      });
-    };
-  })
-  .catch(e => {
-    console.log("JOIN ERROR", e);
-  });
+async function init() {
+  const connection = await new Colyseus.Client("ws://localhost:2567");
 
-const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: false,
-      gravity: { y: 0 }
-    }
-  },
-  scene: [LoadingScene, RegisterScene, LobbyScene, MainGame, EndGameScene],
+  const room = await connection.joinOrCreate("game");
 
-  autoFocus: false
-};
+  const config = {
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 800,
+    height: 600,
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: false,
+        gravity: { y: 0 }
+      }
+    },
+    scene: new MainGame(room),
+
+    autoFocus: false
+  };
+
+  const game = new Phaser.Game(config);
+}
+
+init();
--- server/src/game/scenes/MainGame.ts
+++ server/src/game/scenes/MainGame.ts
@@ -1,12 +1,17 @@
 import { Scene } from "phaser";
 
 export class MainGame extends Scene {
-  public constructor() {
+  protected room: any;
+
+  public constructor(room: any) {
     super({ key: "MainGame" });
+    this.room = room;
   }
 
   public preload() {
     // Load assets here
+    // this.load.image("ship", "../../../assets/spaceShips_001.png");
+    // this.load.image("star", "../../../assets/star_gold.png");
   }
 
   public create() {
