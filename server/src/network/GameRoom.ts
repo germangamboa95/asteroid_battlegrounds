b15e45a580698b74236a9430c600ae6379bd14b7
annaelde <elde.av@gmail.com>
2019-09-29 12:23:51 -0400

    Add lobby room.

--- server/src/network/GameRoom.ts
+++ server/src/network/GameRoom.ts
@@ -7,6 +7,8 @@ class Player extends Schema {
   x: number = 0;
   @type("number")
   y: number = 0;
+  @type("string")
+  name: string = '';
 }
 
 class PlayerMap extends Schema {
@@ -20,8 +22,10 @@ export class State extends Schema {
 
   something = "This attribute won't be sent to the client-side";
 
-  createPlayer(id: string) {
-    this.players[id] = new Player();
+  createPlayer(id: string, name: string) {
+    const player = new Player();
+    player.name = name;
+    this.players[id] = player;
   }
 
   removePlayer(id: string) {
@@ -48,7 +52,7 @@ export class GameRoom extends Room<State> {
   }
 
   onJoin(client: Client, options: any) {
-    this.state.createPlayer(client.sessionId);
+    this.state.createPlayer(client.sessionId, options.name);
   }
 
   onMessage(client: Client, message: any) {
@@ -60,6 +64,7 @@ export class GameRoom extends Room<State> {
     );
     this.state.movePlayer(client.sessionId, message);
   }
+
   onLeave(client: Client, consented: boolean) {
     this.state.removePlayer(client.sessionId);
   }
