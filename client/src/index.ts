b58cb44f8dad3b5de54d7851143371384cb9cde1
Nils Elde <nils.elde@gmail.com>
2019-09-29 13:25:22 -0400

    scale game window

--- client/src/index.ts
+++ client/src/index.ts
@@ -14,6 +14,10 @@ async function init() {
     parent: "container",
     width: 800,
     height: 600,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     pixelArt: true,
     physics: {
       default: "arcade",
